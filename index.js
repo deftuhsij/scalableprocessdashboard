30e9149ed749838cb0238a490ebcc0bf75504784
deftuhsij <esjcrickmorris9@hotmail.com>
2024-05-10 12:17:43 +0700

    update sdk project

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-function coinChange(coins, amount) {
-  const dp = new Array(amount + 1).fill(amount + 1);
-  dp[0] = 0;
-  for (const coin of coins) {
-    for (let i = coin; i <= amount; i++) {
-      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+function countSubstrings(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    extendPalindrome(i, i);
+    extendPalindrome(i, i + 1);
+  }
+  return count;
+  function extendPalindrome(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      count++;
+      left--;
+      right++;
     }
   }
-  return dp[amount] > amount ? -1 : dp[amount];
 }
